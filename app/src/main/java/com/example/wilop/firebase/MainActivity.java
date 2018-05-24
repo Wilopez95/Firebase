98f972cef38fd687c750d2576450df4086b0b91d
Wilopez <Wilopez95@gmail.com>
2018-05-24 00:37:38 -0600

    Third commit

--- app/src/main/java/com/example/wilop/firebase/MainActivity.java
+++ app/src/main/java/com/example/wilop/firebase/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.wilop.firebase;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.renderscript.Sampler;
+import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,9 +17,14 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -24,14 +32,10 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
+import com.google.firebase.storage.UploadTask;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
+
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
@@ -42,14 +46,19 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<String> Names = new ArrayList<String>();
     ArrayList<String> Prices = new ArrayList<String>();
     ArrayList<String> Description = new ArrayList<String>();
-    JSONObject jsonObject;
-    String value;
+    ArrayList<String> Url_Imagenes = new ArrayList<String>();
     Context context = this;
     FloatingActionButton fab;
     FirebaseDatabase database;
     StorageReference mStorageRef;
     DatabaseReference myRef;
     DatabaseReference myData;
+    Uri uri;
+    Intent intent;
+    ProgressDialog mprogress;
+    String URLimage="null";
+    private static final int GALLERY_INTENT = 1;
+
 
     DatabaseReference myDatabasereference;
     @Override
@@ -74,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
         add_product = findViewById(R.id.add_product);
         ListaProductos = findViewById(R.id.list_products);
         fab = findViewById(R.id.fab);
+        mprogress = new ProgressDialog(this);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -92,21 +102,15 @@ public class MainActivity extends AppCompatActivity {
                 if(name.isEmpty()|| price.isEmpty() || description.isEmpty()){
                     Snackbar.make(v, "Invalid data", Snackbar.LENGTH_LONG)
                             .setAction("Action", null).show();
-                }else{
-                    myRef = database.getReference("Products");
-                    Map<String, String> map = new HashMap<>();
-                    map.put("Name",name);
-                    map.put("Price",price);
-                    map.put("Description",description);
-                    myRef.push().setValue(map);
-                    Toast.makeText(getApplicationContext(), "Add new product",
-                            Toast.LENGTH_SHORT).show();
-                    //fab.setVisibility(View.VISIBLE);
-                    //ListaProductos.setVisibility(View.VISIBLE);
-                    //add_product.setVisibility(View.INVISIBLE);
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                    startActivity(intent);
-                }
+                }else {
+                        //Upload the imagen
+                        intent = new Intent(Intent.ACTION_PICK);
+                        intent.setType("image/*");
+                        startActivityForResult(intent,GALLERY_INTENT);
+
+
+                    }
+
             }
         });
 
@@ -128,7 +132,9 @@ public class MainActivity extends AppCompatActivity {
                     Prices.add(price+"₡");
                     String description = ds.child("Description").getValue(String.class);
                     Description.add(description);
-                    Log.d("DATOS", name + " / " + description + " / " + price);
+                    String url = ds.child("Url").getValue(String.class);
+                    Url_Imagenes.add(url);
+                    Log.d("DATOS", name + " / " + description + " / " + price+ " / " +url);
                 }
                 ListaProductos.setAdapter(new Adapter_product(context,Names,Prices,Description));
             }
@@ -140,6 +146,42 @@ public class MainActivity extends AppCompatActivity {
         });
         }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == GALLERY_INTENT && resultCode ==RESULT_OK){
+            mprogress.setTitle("Uploading");
+            mprogress.setMessage("Uploading to firebase");
+            mprogress.setCancelable(false);
+            mprogress.show();
+
+            uri = data.getData();
+            StorageReference filepah = mStorageRef.child("Pictures").child(uri.getLastPathSegment());
+            filepah.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    String name =  Pname.getText().toString();
+                    String price = Pprice.getText().toString();
+                    String description = Pdescription.getText().toString();
+                    mprogress.dismiss();
+                    Uri dowloadphoto = taskSnapshot.getDownloadUrl();
+                    myRef = database.getReference("Products");
+                    Map<String, String> map = new HashMap<>();
+                    map.put("Name", name);
+                    map.put("Price", price);
+                    map.put("Description", description);
+                    map.put("Url",dowloadphoto.toString());
+                    myRef.push().setValue(map);
+
+                    //Hacer al final
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(getApplicationContext(), "New product added",
+                            Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
 
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
